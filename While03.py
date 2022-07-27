03a6e67c4ef82daf3683f05284cb4ac848b9f536
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-28 00:00:45 +0500

    While03.py

--- While03.py
+++ While03.py
@@ -1,3 +1,4 @@
+import string
 def main(s):
     """
     A variable of type str is given. Find how many punctuations it contains and return.
@@ -6,4 +7,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    l = string.punctuation
+    c = 0
+    i = 0
+    while i < len(s):
+        if s[i] in l:
+            c += 1
+        i += 1
+    return c
