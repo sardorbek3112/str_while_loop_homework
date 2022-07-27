d521864ac42a3da3421af1baecb1dff4935b2fb6
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-27 23:54:30 +0500

    While01.py

--- While01.py
+++ While01.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    c = 0
+    i = 0
+    while i < len(s):
+        if s[i].isdigit():
+            c += 1
+        i += 1
+    return c
+
