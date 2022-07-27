0a3617e5bf67c7b0e6f7619b640660bad7fbc247
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-28 00:08:21 +0500

    While10.py

--- While10.py
+++ While10.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    l = '13579'
+    c = 0
+    i = 0
+    while i < len(s):
+        if s[i] in l:
+            c += int(s[i])
+        i += 1
+    return c
